ed3ef436bdd189ada6e49c9c2584b9df4043a020
Hyunseo Lee <hoonhyunseo@gmail.com>
2022-10-24 22:30:06 -0500

    first commit

---
+++ main.swift
@@ -0,0 +1,54 @@
+var words = [String]()
+let newWords: [String]
+
+func swap(strings:inout [String],firstIndex:Int,secondIndex:Int)
+{
+    strings.swapAt(firstIndex,secondIndex)
+}
+func whetherStringIsGreater(string1:String,string2:String) -> Bool
+{
+    return string1.count > string2.count
+}
+func sort(unsortedStrings:[String]) -> [String]
+{
+    var tempCount = 0
+    var totalCount = 0
+    var unsortedArray = unsortedStrings
+    for i in 0 ..< unsortedArray.count - 1
+    {
+        var index = i+1
+        for _ in 1 ..< i+2
+        {
+            if(whetherStringIsGreater(string1:unsortedArray[index-1],string2:unsortedArray[index]))
+            {
+                tempCount += 1
+                totalCount += 1
+                swap(strings:&unsortedArray,firstIndex:index,secondIndex:index-1)
+                index -= 1
+            }
+            else
+            {
+                break
+            }
+        }
+        tempCount = 0
+    }
+    return unsortedArray
+}
+while let word = readLine()
+{
+    if word == ""
+    {
+        break
+    }
+    else
+    {
+        words.append(word)
+    }
+}
+newWords = sort(unsortedStrings:words)
+print("========== The Result ==========")
+for word in newWords
+{
+    print(word)
+}
